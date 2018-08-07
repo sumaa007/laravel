20a38df447b716db63e0151b2751b229b15b11ca
sumaa007 <liujiezr@foxmail>
2018-08-07 15:07:46 +0800

    :construction: add userer_权限管理

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -16,10 +16,11 @@ class UsersTableSeeder extends Seeder
         User::insert($users->makeVisible(['password', 'remember_token'])->toArray());
 
         $user = User::find(1);
-        $user->name = 'Aufree';
-        $user->email = 'aufree@yousails.com';
+        $user->name = 'sum';
+        $user->email = 'sum@sum.com';
         $user->password = bcrypt('password');
         $user->is_admin = true;
+        $user->activated = true;
         $user->save();
     }
 }
